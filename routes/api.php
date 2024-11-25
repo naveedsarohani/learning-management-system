1b6490ff32000605842a268e0efd713053471cda
Naveed <naveed.sarohani@gmail.com>
2024-11-25 16:42:45 +0500

    Submission API developement was completed

--- routes/api.php
+++ routes/api.php
@@ -10,17 +10,17 @@ use Illuminate\Support\Facades\Route;
 Route::controller(UserController::class)->prefix('auth')->group(function () {
     Route::post('/login', 'validateLogin');
     Route::post('/register', 'register');
+});
 
-    Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('auth')->group(function () {
         Route::get('/users', 'index');
         Route::get('/logout', 'invalidateLogin');
         Route::put('/update/{id}', 'update');
         Route::delete('/delete/{user}', 'delete');
         Route::post('/update-password/{id}', 'updatePassword');
     });
-});
 
-Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('assessments', AssessmentController::class)->except(['index', 'show', 'update']);
     Route::apiResource('submissions', SubmissionController::class)->only(['index', 'show']);
 
